d28653b4ab3b7a0a1f6341cf052f02433abbb79d
Tomas Escudero <tomyesq04@gmail.com>
2025-07-22 13:29:54 -0300

    Initial config

---
+++ server/index.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import http from 'http';
+import {Server as SocketServer} from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new SocketServer(server);
