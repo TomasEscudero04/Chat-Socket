5c31c1e0ed5fae61b3998de54a47775b3c9db954
Tomas Escudero <tomyesq04@gmail.com>
2025-10-22 13:22:36 -0300

    Cambio en el puerto para render

--- server/index.js
+++ server/index.js
@@ -62,7 +62,8 @@ io.on('connection', socket => {
 });
 
 const PORT = process.env.PORT || 3000;
+const HOST = '0.0.0.0';
 
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+server.listen(PORT, HOST, () => {
+  console.log(`Server listening on ${HOST}:${PORT}`);
 });
